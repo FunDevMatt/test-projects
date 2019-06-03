1046653cf736cebd6148ab7f968d3498be43e2f6
matthew rich <rich.matthewJ@gmail.com>
2019-06-02 22:40:05 -0500

    optimal solution two sums

--- algo-expert-problems/two-number-sum/two-number-sum.js
+++ algo-expert-problems/two-number-sum/two-number-sum.js
@@ -1,21 +1,37 @@
+// function twoNumberSum(array, targetSum) {
+//     let answer = [];
+//     array.forEach(num => {
+//         let numForGoal = targetSum - num;
+//         let newArray = [...array];
+//         let indexToRemove = newArray.indexOf(num);
+//         newArray.splice(indexToRemove, 1);
+//         if (newArray.includes(numForGoal)) {
+//             answer = [num, numForGoal]
+//         }
+//     })
+//     if (answer.length === 0) {
+//         return "no matches"
+//     }
+//     return answer.sort((a, b) => a - b);
+// }
+
+// optimal solutuon | time = o(nlog(n)) | space = o(1)
 function twoNumberSum(array, targetSum) {
-    let answer = [];
-    array.forEach(num => {
-        let numForGoal = targetSum - num;
-        let newArray = [...array];
-        let indexToRemove = newArray.indexOf(num);
-        newArray.splice(indexToRemove, 1);
-        if (newArray.includes(numForGoal)) {
-            answer = [num, numForGoal]
+    array.sort((a, b) => a - b);
+    let left = 0;
+    let right = array.length - 1;
+    while (left < right) {
+        let currentSum = array[left] + array[right];
+        if (currentSum === targetSum) {
+            return [array[left], array[right]]
+        } else if (currentSum < targetSum) {
+            left++
+        } else if (currentSum > targetSum) {
+            right--
         }
-    })
-    if (answer.length === 0) {
-        return "no matches"
     }
-    return answer.sort((a, b) => a - b);
+    return "no matches"
 }
 
 module.exports = twoNumberSum;
 
-
-console.log(twoNumberSum([1, 2, 3, 4, 4], 8))
